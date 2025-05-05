615c9f54269fa3507843a233c9c96538ad4feda5
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-05-05 14:09:27 +0500

    fix: pin to nix branch

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     xinux-lib = {
       url = "github:xinux-org/lib";
       inputs.nixpkgs.follows = "nixpkgs";
