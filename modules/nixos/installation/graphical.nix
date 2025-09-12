f6a4d40d18cc389e8116918aa1e3382ed7fbea4a
BeMeritus <bemerituss@gmail.com>
2025-09-12 19:07:04 +0500

    xserver removed and moved unstable (#23)
    * chore: moved unstable and flake update
    
    * fix: removed xserver
    
    * chore: update flake.lock

--- modules/nixos/installation/graphical.nix
+++ modules/nixos/installation/graphical.nix
@@ -5,7 +5,7 @@ in
 {
   xeonitte.enable = true;
 
-  services.xserver.desktopManager.gnome = {
+  services.desktopManager.gnome = {
     # Add Firefox and other tools useful for installation to the launcher
     favoriteAppsOverride = ''
       [org.gnome.shell]
@@ -33,7 +33,7 @@ in
     };
   };
 
-  services.xserver.displayManager = {
+  services.displayManager = {
     gdm = {
       enable = true;
       # autoSuspend makes the machine automatically suspend after inactivity.
