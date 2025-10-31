0785a134131f5752e619870419ab5d8a7de8fda8
bahrom04 <magdiyevbahrom@gmail.com>
2025-10-31 19:30:46 +0500

    chore: revert to stable

--- modules/nixos/installation/graphical.nix
+++ modules/nixos/installation/graphical.nix
@@ -13,7 +13,7 @@
 in {
   xeonitte.enable = true;
 
-  services.desktopManager.gnome = {
+  services.xserver.desktopManager.gnome = {
     # Add Firefox and other tools useful for installation to the launcher
     favoriteAppsOverride = ''
       [org.gnome.shell]
@@ -41,7 +41,7 @@ in {
     };
   };
 
-  services.displayManager = {
+  services.xserver.displayManager = {
     gdm = {
       enable = true;
       # autoSuspend makes the machine automatically suspend after inactivity.
