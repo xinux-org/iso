d20ec563da3011aac9b2d9ddb66356a98050ba17
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2026-02-13 00:38:17 +0500

    fix: module reference to debug

--- systems/x86_64-linux/xinux/default.nix
+++ systems/x86_64-linux/xinux/default.nix
@@ -1,6 +1,9 @@
 { inputs, system, pkgs, config, lib, xinux, ... }:
 
 {
-  modules.xinux.debug = true;
+  # Enable debug mode for development purposes
+  xinux.debug.enable = true;
+
+  # Don't install Xinux Module Manager by default
   modules.xinux.xinuxModuleManager.enable = false;
 }
