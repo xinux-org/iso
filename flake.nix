be9a35dfef8634669235f9e64e96c1bd1be95961
bahrom04 <magdiyevbahrom@gmail.com>
2026-04-26 16:14:42 +0500

    chore: add reference for future tasks

--- flake.nix
+++ flake.nix
@@ -47,6 +47,8 @@
       src = ./.;
       alias.shells.default = "iso";
       packages = {
+        # after writing iso disk, volumeID should be also xinux instead of nixos. See below
+        # https://github.com/xinux-org/nixpkgs/blob/1167ddc8c033d28b9d07afccba2708af1f73cfc1/nixos/modules/installer/cd-dvd/iso-image.nix#L588
         x86_64-linux.xinux =
           inputs.self.nixosConfigurations.xinux.config.system.build.images.iso-installer.overrideAttrs
             (oldAttrs: {
