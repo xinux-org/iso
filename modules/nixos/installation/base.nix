11b73abee17abdafdf175c19a0119e94801bc606
bahrom04 <magdiyevbahrom@gmail.com>
2026-07-01 17:57:21 +0500

    remove duplicate nixos cache

--- modules/nixos/installation/base.nix
+++ modules/nixos/installation/base.nix
@@ -40,11 +40,9 @@ in
       ];
       substituters = [
         "https://cache.xinux.uz/?priority=100"
-        "https://cache.nixos.org/"
       ];
-      trusted-public-keys = [
-        "cache.xinux.uz:BXCrtqejFjWzWEB9YuGB7X2MV4ttBur1N8BkwQRdH+0=" # xinux
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=" # nixos
+      trusted-public-keys = lib.mkBefore [
+        "cache.xinux.uz:BXCrtqejFjWzWEB9YuGB7X2MV4ttBur1N8BkwQRdH+0="
       ];
     };
   };
